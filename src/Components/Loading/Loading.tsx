692c29785b3f8b0dd6ffba97a5270a97baa3adfd
Marwanaashraf <marwanelmaghraby45@gmail.com>
2025-10-23 02:51:50 +0300

    handle loading commit

--- src/Components/Loading/Loading.tsx
+++ src/Components/Loading/Loading.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { Spiner } from '../../Icons'
-
+import React from "react";
+import { Spiner } from "../../Icons";
+import logo from "../../assets/Images/logo/dd_nav_logo.svg";
+import { motion } from "framer-motion";
 export default function Loading() {
   return (
-    <div className='fixed top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center  bg-slate-100 dark:bg-slate-800'>
-      <Spiner className='w-16 h-16'/>
+    <div className="fixed top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center  bg-slate-100 dark:bg-slate-800">
+      {/* <Spiner className='w-16 h-16'/> */}
+      <motion.img
+        initial={{ opacity: 0.5, scale: 0.99 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.3, repeat: Infinity, repeatType: "reverse" }}
+        className="w-60"
+        src={logo}
+        alt="logo"
+      />
     </div>
-  )
+  );
 }
